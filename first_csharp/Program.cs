3bb020b9106b662662cbc44fd67863b84bad3292
Chris Ulanowicz <src3collector@gmail.com>
2017-08-28 12:45:30 -0500

    added typical Hello World

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -6,13 +6,7 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            bool test = true;
-            for(int i = 1;i<256;i++)
-            {
-                Console.WriteLine(i);
-            }
             Console.WriteLine("Hello CasualtyITC!");
-            Console.WriteLine(test);
         }
     }
 }
